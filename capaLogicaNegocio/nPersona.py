a65499b1435fff31d439cd489a364172cdba0046
i2513383-star <i2513383@continental.edu.pe>
2026-01-07 13:20:39 -0500

    Update nPersona.py

--- capaLogicaNegocio/nPersona.py
+++ capaLogicaNegocio/nPersona.py
@@ -14,4 +14,23 @@ class nPersona:
         return self.__dPersona.actualizarPersona(persona,docIdentidad)
     
     def eliminarPersona(self,docIdentidad:str):
+
         return self.__dPersona.eliminarPersona(docIdentidad)
+
+from capaDatos.dProducto import DProducto
+
+class nProducto:
+    def __init__(self):
+        self.__dProducto = DProducto()
+
+    def mostrarProductos(self):
+        return self.__dProducto.mostrarProductos()
+
+    def nuevoProducto(self, producto: dict):
+        self.__dProducto.nuevoProducto(producto)
+
+    def actualizarProducto(self, producto: dict, id_producto: int):
+        return self.__dProducto.actualizarProducto(producto, id_producto)
+
+    def eliminarProducto(self, id_producto: int):
+        return self.__dProducto.eliminarProducto(id_producto)
